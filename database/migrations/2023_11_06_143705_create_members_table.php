1641ee6e64a6e87b39c3175ba0ef175b5ecdbe0f
Rosyieed <rosyid123.ren@gmail.com>
2023-11-06 16:21:27 +0700

    feature: add migration and seeder

--- database/migrations/2023_11_06_143705_create_members_table.php
+++ database/migrations/2023_11_06_143705_create_members_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['Super Admin','Admin','Cashier','Ironer','Packer'])->default('Cashier');
-            $table->rememberToken();
+            $table->string('email');
+            $table->unsignedBigInteger('phone_number');
+            $table->integer('total_Point');
+            $table->dateTime('registration_date');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 };
---
+++ database/migrations/2023_11_06_145560_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('invoice');
+            $table->string('customer_name', 25);
+            $table->dateTime('order_in');
+            $table->dateTime('order_out');
+            $table->dateTime('order_take');
+            $table->float('total_weight');
+            $table->float('total_price');
+            $table->enum('status', ['Dalam Antrian','Belum dicuci','Sedang dicuci','Sudah Dicuci','Belum disetrika','Sedang disetrika','Sudah disetrika','Sedang dipacking','Selesai'])->default('Dalam Antrian');
+            $table->foreignId('service_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('member_id')->constrained();
+            $table->foreignId('log_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
